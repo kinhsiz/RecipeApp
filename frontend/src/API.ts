f172c3ce45bf9d88d3d22ed38a89d6e0896d25a2
Ruben Rios <rriosal@protonmail.com>
2024-07-02 14:15:30 -0500

    Update API.ts
    Change localhost from 5000 to 3000

--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -2,7 +2,7 @@ import { Recipe } from "./types";
 
 //Builds the searching URL with the term to be searched and number
 export const searchRecipes = async (searchTerm: string, page: number) => {
-  const baseUrl = new URL("http://localhost:5000/api/recipes/search");
+  const baseUrl = new URL("http://localhost:3000/api/recipes/search");
   baseUrl.searchParams.append("searchTerm", searchTerm);
   baseUrl.searchParams.append("page", String(page));
 
@@ -15,7 +15,7 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
 };
 
 export const getRecipeSummary = async(recipeId: string) => {
-  const baseUrl = new URL(`http://localhost:5000/api/recipes/${recipeId}/summary`); //ticks to replace recipeID, instead of quotes. 
+  const baseUrl = new URL(`http://localhost:3000/api/recipes/${recipeId}/summary`); //ticks to replace recipeID, instead of quotes. 
 
   const response = await fetch(baseUrl);
   if(!response.ok) {
@@ -25,7 +25,7 @@ export const getRecipeSummary = async(recipeId: string) => {
 };
 
 export const addFavoriteRecipe = async(recipe : Recipe) => {
-  const baseUrl = new URL("http://localhost:5000/api/recipes/favourite");
+  const baseUrl = new URL("http://localhost:3000/api/recipes/favourite");
   const body = {recipeId : recipe.id};
 
   const response = await fetch(baseUrl, {
@@ -42,7 +42,7 @@ export const addFavoriteRecipe = async(recipe : Recipe) => {
 }
 
 export const getFavoriteRecipes = async() => {
-  const baseUrl = new URL("http://localhost:5000/api/recipes/favourite");
+  const baseUrl = new URL("http://localhost:3000/api/recipes/favourite");
   const response = await fetch(baseUrl);
   if(!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
@@ -51,7 +51,7 @@ export const getFavoriteRecipes = async() => {
 };
 
 export const removeFavRecipe = async(recipe : Recipe) => {
-  const baseUrl = new URL("http://localhost:5000/api/recipes/favourite");
+  const baseUrl = new URL("http://localhost:3000/api/recipes/favourite");
   const body = {recipeId : recipe.id};
 
   const response = await fetch(baseUrl, {
